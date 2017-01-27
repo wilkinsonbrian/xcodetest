7f20b23d175094fa1ad542313c269ea3986ae1e9
admin <admin@imac01.local>
2017-01-27 13:45:26 -0500

    Added comment to ViewController

--- GitHibXcodeTest/ViewController.swift
+++ GitHibXcodeTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // This is where I would write some important code
     }
 
     override func didReceiveMemoryWarning() {
